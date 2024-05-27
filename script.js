53ee3f63573a7bcc19ab453ef5d5cae6d1bfa821
tatomika <gvantsajikhvadze93@gmail.com>
2024-05-27 23:56:32 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function myLastName() {
+    let myName = document.getElementById("title1");
+    console.log(myName.textContent);
+    myName.textContent = "Dolidze";
+}
+function myFirstName() {
+    let myName = document.getElementById("title1");
+    console.log(myName.textContent);
+    myName.textContent = "Gvantsa";
+}
