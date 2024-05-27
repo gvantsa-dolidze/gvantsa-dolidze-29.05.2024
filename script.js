dcc1dc34466a557a7b58025fda9f3e9e74a075d4
tatomika <gvantsajikhvadze93@gmail.com>
2024-05-28 00:07:26 +0400

    changes

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
+let myName = document.getElementById("title");
+console.log(myName.textContent);
 function myLastName() {
-    let myName = document.getElementById("title");
-    console.log(myName.textContent);
     myName.textContent = "Dolidze";
 }
 function myFirstName() {
-    let myName = document.getElementById("title");
-    console.log(myName.textContent);
     myName.textContent = "Gvantsa";
 }
